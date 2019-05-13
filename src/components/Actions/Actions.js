2d3bf82058133a29213e8aae0ec699ec4f882813
Paul Greenham <paul.greenham@gmail.com>
2019-05-13 11:24:08 +0300

    working on actions - search bar

--- src/components/Actions/Actions.js
+++ src/components/Actions/Actions.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import SearchClients from './SearchClients'
 import UpdateClient from './UpdateClient'
 import AddClient from './AddClient'
+import axios from 'axios'
 
 import '../../style/actions.css'
 
@@ -9,19 +10,27 @@ class Actions extends Component {
     constructor () {
         super()
         this.state = {
-            client: {}
+            clients: []
         }
     }
 
-    componentDidMount = () => {
+    updateClientInfo = async () => {
+        let dbclients = await axios.get('http://localhost:3723/clientdata')
+        this.setState({
+            clients: dbclients.data
+        })
+    }
+
+    async componentDidMount() {
         this.props.changePage("Actions")
+        await this.updateClientInfo()
     }
 
     render(){
         return (<div className="actions-container">
             <div className="update-client">
                 <h1>UPDATE</h1>
-                <SearchClients />
+                <SearchClients clients={this.state.clients}/>
                 <UpdateClient />
             </div>
             <div className="add-client">
--- src/components/Actions/SearchClients.js
+++ src/components/Actions/SearchClients.js
@@ -4,15 +4,32 @@ class SearchClients extends Component {
     constructor() {
         super()
         this.state = {
-            client: {}
+            searchName: ""
         }
+    }
+
+    filterClient = client => client.name.toLowerCase().includes(this.state.searchName.toLowerCase())
+
+    getSearchedClients = () => this.props.clients.filter(c => this.filterClient(c))
+
+    populateClientSearch = () => {
+        return (<datalist id="clientNames">
+            {this.getSearchedClients().map(c => <option value={c.name} key={c._id} />)}
+        </datalist>)
+    }
+
+    chooseClient = event => {
+        this.setState({ searchName: event.target.value })
     }
 
     render(){
         return (<div>
-            Search Clients
+            Client: <input list="clientNames" value={this.state.searchName} onInput={this.chooseClient} />
+            {this.populateClientSearch()}
         </div>)
     }
 }
 
 export default SearchClients
+
+// type="text" value={this.state.searchName} onChange={this.chooseClient}
